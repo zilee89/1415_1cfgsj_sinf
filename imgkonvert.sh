4f018bbdc127a6e7fb5d93c6182eaac0296d4144
juanjokalea <juanjokalea@hotmail.com>
2015-04-21 13:14:45 +0200

    modificacion1_JJ

--- imgkonvert.sh
+++ imgkonvert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Prueba modificacion JUANJO
 # Script to convert all mage files from a directory (parameter) into png format file
 # it also reduces size
 
@@ -8,13 +8,13 @@
 
 # Main code
 
-ls -1 $1*.jpg > imagefiles
-
+ls -1 $1/*.jpg > imagefiles
+cat imagefiles
 while read imgfile 
 do
 	lengthname=${#imgfile}
-	namefile=${imgfile;0;$lengthname-4}
-	convert $imgfile -resize 200x200 $namefile.png	
+	namefile=${imgfile:0:$lengthname-4}
+	convert "$1/imgfile" -resize 200x200 "$namefile".png	
 
 done < imagefiles
 rm imagefiles
